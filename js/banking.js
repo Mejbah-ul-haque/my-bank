480c5b58eadefdfc87eae774c653a406b7621891
Mejbah <mejba.s@gmail.com>
2022-02-12 03:55:51 +0600

    finish

--- js/banking.js
+++ js/banking.js
@@ -1,39 +1,31 @@
-
-
-
 function getInputValue(itputId){
     const inputField = document.getElementById(itputId);
     const amountValue = parseFloat(inputField.value);
     inputField.value = '';
     return amountValue;
 }
-
 function updateTotalField(totalFieldId, amount){
     const totalElement = document.getElementById(totalFieldId);
     const previousTotal = parseFloat(totalElement.innerText);
     totalElement.innerText = previousTotal + amount;
 }
-
+function updateBalance(amount, isAdd){
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalance = parseFloat(balanceTotal.innerText);
+    if(isAdd == true){
+        balanceTotal.innerText = previousBalance + amount;
+    }
+    else{
+       balanceTotal.innerText = previousBalance - amount; 
+    }   
+}
 document.getElementById('deposit-button').addEventListener('click', function(){
-
     const amount = getInputValue('deposit-input');
-      
     updateTotalField('deposit-total', amount);
-
-    //balance section
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBalance = parseFloat(balanceTotal.innerText);
-    balanceTotal.innerText = previousBalance + amount;
+    updateBalance(amount, true);
 })
-
 document.getElementById('withdraw-button').addEventListener('click', function(){
-
-    const newWithdraw = getInputValue('withdraw-input');
-
-    updateTotalField('withdraw-total', newWithdraw);
-
-    // Rest of Balance
-    const balanceTotal = document.getElementById('balance-total');
-    const previousBalance = parseFloat(balanceTotal.innerText);
-    balanceTotal.innerText = previousBalance - newWithdraw;
+    const amount = getInputValue('withdraw-input');
+    updateTotalField('withdraw-total', amount);
+    updateBalance(amount, false);
 })
